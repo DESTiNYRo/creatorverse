ece4ec38879d28455932a5b1d24b7e9a001c877a
DESTiNYRo <destinyrosado01@gmail.com>
2026-08-24 21:45:20 -0400

    supabase Creation

---
+++ src/client.js
@@ -0,0 +1,7 @@
+import {createClient} from '@supabase/supabase-js'
+
+const URL = 'https://cfslmqmwrocezbchhqwa.supabase.co'
+const API_KEY = 'cfslmqmwrocezbchhqwa'
+const supabase = createClient(URL, API_KEY)
+
+export const supabase = createClient(URL, API_KEY)
